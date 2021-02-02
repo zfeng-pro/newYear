0fd2e431a643d12bc57590e27f7436bb743d17fd
zf <1581745050@qq.com>
2021-02-02 17:03:44 +0800

    选择背景

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,16 +8,21 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    codeimg: "",
-    // 默认虚拟数据
-    cardBase: {
-      //需要https图片路径,下载到本地然后去绘制
-      cardbg: "../../img/viewBg.png",
-      // 二维码
-      codeImg: "https://7265-redux-32ab9a-1259022487.tcb.qcloud.la/image/111.png",
-      // 用户头像
-      headImg:null
-    }
+    bless: '',
+    showCanvas: false,
+    // 二维码
+    codeImg: "../../img/codeImg.png",
+    bgList: [
+      'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-0c03b75a-8139-4654-83b3-f12d36df4bbe/3a5ec1d6-7a31-4e6f-b60b-aa7f3a0d83d2.png',
+      'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-0c03b75a-8139-4654-83b3-f12d36df4bbe/e3dfcba9-2ab5-4ee4-90af-7c8e66adfbf3.png',
+      'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-0c03b75a-8139-4654-83b3-f12d36df4bbe/ebde0372-be48-4aeb-afc5-40e26937701d.png',
+      'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-0c03b75a-8139-4654-83b3-f12d36df4bbe/e94fa668-5738-4dc9-a1b6-dba3b26299a7.png'
+    ],
+    cardbg: "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-0c03b75a-8139-4654-83b3-f12d36df4bbe/e94fa668-5738-4dc9-a1b6-dba3b26299a7.png",
+    // 用户头像
+    headImg: null,
+    showAllBg: false,
+
   },
   // 事件处理函数
   bindViewTap() {
@@ -25,34 +30,61 @@ Page({
   },
   onLoad() {
     let that = this;
-    let cardBase = that.data.cardBase;
     // 在没有 open-type=getUserInfo 版本的兼容处理
     wx.getUserInfo({
       success: res => {
         app.globalData.userInfo = res.userInfo
         that.userInfo = res.userInfo;
+        that.data.userInfo = res.userInfo;
         console.log(that.userInfo)
+        // 下载头像
         wx.downloadFile({
-          url:  that.userInfo.avatarUrl,
-          success: function(res) {
+          url: res.userInfo.avatarUrl,
+          success: function (res) {
             wx.hideLoading();
             if (res.statusCode === 200) {
-              that.headImg = res.tempFilePath;
-              that.getCanvas(cardBase.cardbg, cardBase.codeImg, headImg);
+              that.data.headImg = res.tempFilePath;
+              // that.getCanvas(that.data.cardbg, that.data.codeImg, res.tempFilePath);
             }
           }
         })
-        
+
       }
     })
   },
-
+  // 查看全部背景
+  getAllBg() {
+    this.data.showAllBg = !this.data.showAllBg;
+    this.setData({
+      showAllBg: this.data.showAllBg
+    })
+  },
+  // 更改背景
+  changeBg(e){
+    let bg = e.currentTarget.dataset['bg'];
+    this.data.cardbg = bg;
+    this.setData({
+      cardbg: this.data.cardbg
+    })
+  },
   /**
    * 开始用canvas绘制分享海报
    * @param cardbg 下载的海报背景图路径
    * @param codeImg   下载的二维码图片路径
    */
-  getCanvas: function (cardbg, codeImg, headImg) {
+  getCanvas: function (cardbgurl, codeImg, headImg) {
+    let cardbg;
+    // 下载背景
+    wx.downloadFile({
+      url: cardbgurl,
+      success: function (res) {
+        wx.hideLoading();
+        if (res.statusCode === 200) {
+          cardbg = res.tempFilePath;
+        }
+      }
+    })
+
     wx.showLoading({
       title: '正在生成中...',
       mask: true,
@@ -81,14 +113,27 @@ Page({
       if (cardbg) {
         ctx.drawImage(cardbg, -2 * rpx, -4 * rpx, 379 * rpx, 706 * rpx);
       }
-      // 标题
+
+      // 内容
+      ctx.setFontSize(15);
+      ctx.setFillStyle('#000');
+      ctx.setTextAlign('left');
+      ctx.fillText("前端/小程序开发前端/小程序开发前端/小程序开发前端/小程序开发前端/小程序开发前端/小程序开发微信号:qq287534291", 35 * rpx, 415 * rpx, 100 * rpx, 100 * rpx); //姓名
+
+
+      //  绘制二维码
+      console.log(codeImg)
+      if (codeImg) {
+        ctx.drawImage(codeImg, 165 * rpx, 320 * rpx, 100 * rpx, 100 * rpx)
+      }
+
+      // 姓名
       ctx.setFontSize(14);
       ctx.setFillStyle('#000');
       ctx.setTextAlign('left');
       ctx.fillText(that.userInfo.nickName, 35 * rpx, 355 * rpx, 100 * rpx, 100 * rpx); //姓名
-      console.log(headImg)
-     
-      // ctx.drawImage(headImg, 50 * rpx, 220 * rpx, 20 * rpx, 20 * rpx);//头像
+
+      //头像
       let avatarurl_width = 60, //绘制的头像宽度
         avatarurl_heigth = 60, //绘制的头像高度
         avatarurl_x = 28, //绘制的头像在画布上的位置
@@ -100,20 +145,6 @@ Page({
       ctx.arc(avatarurl_width / 2 + avatarurl_x, avatarurl_heigth / 2 + avatarurl_y, avatarurl_width / 2, 0, Math.PI * 2, false);
       ctx.clip(); //画了圆 再剪切  原始画布中剪切任意形状和尺寸。一旦剪切了某个区域，则所有之后的绘图都会被限制在被剪切的区域内
       ctx.drawImage(headImg, avatarurl_x, avatarurl_y, avatarurl_width, avatarurl_heigth); // 推进去图片
-      // 标题
-      ctx.setFontSize(15);
-      ctx.setFillStyle('#000');
-      ctx.setTextAlign('left');
-      ctx.fillText("前端/小程序开发", 35 * rpx, 385 * rpx, 100 * rpx, 100 * rpx); //姓名
-      // 标题
-      ctx.setFontSize(15);
-      ctx.setFillStyle('#000');
-      ctx.setTextAlign('left');
-      ctx.fillText("微信号:qq287534291", 35 * rpx, 415 * rpx, 100 * rpx, 100 * rpx); //姓名
-      //  绘制二维码
-      if (codeImg) {
-        ctx.drawImage(codeImg, 165 * rpx, 320 * rpx, 100 * rpx, 100 * rpx)
-      }
     }).exec()
 
     setTimeout(function () {
